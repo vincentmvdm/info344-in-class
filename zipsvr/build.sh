7d2dfb002ed1736b70c3f0ae8882efbeac283529
Dave Stearns <dlsinfo@uw.edu>
2017-10-10 15:19:56 -0700

    end of class

--- zipsvr/build.sh
+++ zipsvr/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 echo "building go server for Linux..."
 #Linux users, execut: CGO_ENABLED=0 go build -a
 GOOS=linux go build 
--- zipsvr/run.sh
+++ zipsvr/run.sh
@@ -4,7 +4,7 @@ docker rm -f zipsvr
 docker run -d \
 -p 443:443 \
 --name zipsvr \
--v /Users/iguest/go/src/github.com/info344-a17/info344-in-class/zipsvr/tls:/tls:ro \
+-v $(pwd)/tls:/tls:ro \
 -e TLSCERT=/tls/fullchain.pem \
 -e TLSKEY=/tls/privkey.pem \
 drstearns/zipsvr
